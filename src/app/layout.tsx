7b7f34d54be600a3833d00e773dbb0af9788a0cc
Chris Parsons <chris@chris-parsons.com>
2023-08-15 22:49:48 -0700

    Get navbar working.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,33 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { NextAuthProvider } from './providers'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { NextAuthProvider } from './providers';
+import NavBar from '@/src/components/navbar';
 
 export const metadata = {
   title: 'Vercel Postgres Demo with Prisma',
   description:
     'A simple Next.js app with Vercel Postgres as the database and Prisma as the ORM',
-}
+};
 
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
   display: 'swap',
-})
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.variable}>
         <NextAuthProvider>
+          <NavBar />
           {children}
         </NextAuthProvider>
       </body>
     </html>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,25 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'black-pearl': {
+          50: '#ecfaff',
+          100: '#d0effd',
+          200: '#a7dffa',
+          300: '#6bc7f5',
+          400: '#27a5e9',
+          500: '#0b88cf',
+          600: '#0c6bae',
+          700: '#11578d',
+          800: '#174973',
+          900: '#183d61',
+          950: '#02080e',
+        },
+      },
       fontFamily: {
         default: ['var(--font-inter)'],
       },
     },
   },
   plugins: [],
-}
+};
